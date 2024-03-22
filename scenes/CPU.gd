52b960d4c8b4810ff90726ce74348796d62866dd
KleberFilho1 <kleirtonabreufilho@gmail.com>
2024-03-22 11:16:47 -0300

    Exportando o projeto para o github

---
+++ scenes/CPU.gd
@@ -0,0 +1,29 @@
+extends StaticBody2D
+
+#IA do Adversário
+var ball_pos : Vector2
+var dist : int
+var move_by : int
+var win_height : int
+var p_height : int
+
+#Configurações do Computador
+func _ready():
+	win_height = get_viewport_rect().size.y
+	p_height = $ColorRect.get_size().y
+
+
+func _process(delta):
+	
+	ball_pos = $"../Ball".position
+	dist = position.y - ball_pos.y
+	
+	if abs(dist) > get_parent().PADDLE_SPEED * delta:
+		move_by = get_parent().PADDLE_SPEED * delta * (dist / abs(dist))
+	else:
+		move_by = dist
+
+	position.y -= move_by
+	
+	
+	position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)
